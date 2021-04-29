3dd9a869e8b626d25476cc397be5f9e9fa989969
Jeffrey Chen <jchen39@cps.edu>
2021-04-29 09:48:38 -0500

    formatted the code for the register page

--- src/register.js
+++ src/register.js
@@ -23,36 +23,44 @@ async function storeUser(name, email, password, done) {
 }
 
 async function registerUser(req, res) {
-	if(req.body.password == req.body.confirmPassword) {
-		const name = req.body.displayName;
-		const email = req.body.email;
-		const password = req.body.password;
-		if(emailValidator.validate(email)) {
-			try {
-				const existingUser = await mongooseModels.users.findOne({ email: email });
-				if(existingUser == null) {
-					storeUser(name, email, password, (error, result) => {
-						if(error) {
-							res.redirect("/login");
-						} else {
-							req.flash("error", result);
-							res.redirect("/register");
-						}
-					});
-				} else {
-					req.flash("error", "username already exist");
-					res.redirect("/register");
-				}
-			} catch(error) {
-				req.flash("error", "unexpected error");
-				res.redirect("/register");
-			}
-		} else {
+	const name = req.body.displayName;
+	const email = req.body.email;
+	const password = req.body.password;
+
+	try {
+		//check if password matches confirm password
+		if(req.body.password != req.body.confirmPassword) {
+			req.flash("error", "passwords do not match");
+			res.redirect("/register");
+			return;
+		}
+
+		//check if email is valid
+		if(!emailValidator.validate(email)) {
 			req.flash("error", "invalid email");
 			res.redirect("/register");
+			return;
 		}
-	} else {
-		req.flash("error", "passwords do not match");
+
+		//check if the user already exist
+		const existingUser = await mongooseModels.users.findOne({ email: email });
+		if(existingUser != null) {
+			req.flash("error", "user already exist");
+			res.redirect("/register");
+			return;
+		}
+
+		//store the user
+		storeUser(name, email, password, (error, result) => {
+			if(error) {
+				res.redirect("/login");
+			} else {
+				req.flash("error", result);
+				res.redirect("/register");
+			}
+		});
+	} catch(error) {
+		res.flash("error", "username already exist");
 		res.redirect("/register");
 	}
 }
